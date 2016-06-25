a89e2a8c323d14a4108eed864f6943587f4f12d4
Knifknif <wernnoken@gmail.com>
2016-06-25 14:56:23 -0400

    added function urlget()

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
+import requests
+import urllib
+
 def saveFile(filename, filecontents):
 	f = open(filename, 'w')
 	f.write(filecontents)
 	f.close()
 
-
+def urlget(site):
+	reqq = urllib.request.urlopen(site)
+	reqqs = reqq.read()
+	return(reqqs)
